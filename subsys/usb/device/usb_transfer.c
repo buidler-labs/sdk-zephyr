d79abca37de3957d4e73797a948d488af037e1cd
Victor ADASCALITEI <victor.adascalitei@3vs.ro>
2024-09-10 16:16:27 +0300

    Reverted USB ZLP response patch

--- subsys/usb/device/usb_transfer.c
+++ subsys/usb/device/usb_transfer.c
@@ -110,11 +110,6 @@ static void usb_transfer_work(struct k_work *item)
 		trans->buffer += bytes;
 		trans->bsize -= bytes;
 		trans->tsize += bytes;
-
-		if (!trans->bsize && trans->flags & USB_TRANS_NO_ZLP) {
-			trans->status = 0;
-			goto done;
-		}
 	} else {
 		ret = usb_dc_ep_read_wait(ep, trans->buffer, trans->bsize,
 					  &bytes);
